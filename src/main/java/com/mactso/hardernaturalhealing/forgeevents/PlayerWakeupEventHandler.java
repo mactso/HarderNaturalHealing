3045610a4aeda94f4d9aaa663fe9600bd2951118
Mac <Mac@Coke.lan>
2024-02-21 17:07:23 -0600

    Version 14 - Fix heal on waking bug

--- src/main/java/com/mactso/hardernaturalhealing/forgeevents/PlayerWakeupEventHandler.java
+++ src/main/java/com/mactso/hardernaturalhealing/forgeevents/PlayerWakeupEventHandler.java
@@ -2,6 +2,7 @@ package com.mactso.hardernaturalhealing.forgeevents;
 
 import com.mactso.hardernaturalhealing.config.MyConfig;
 
+import net.minecraft.world.entity.player.Player;
 import net.minecraftforge.event.entity.player.PlayerWakeUpEvent;
 import net.minecraftforge.eventbus.api.SubscribeEvent;
 import net.minecraftforge.fml.common.Mod;
@@ -9,13 +10,19 @@ import net.minecraftforge.fml.common.Mod;
 @Mod.EventBusSubscriber()
 public class PlayerWakeupEventHandler {
 
-	@SuppressWarnings("resource")
+//	@SuppressWarnings("resource")
 	@SubscribeEvent
 	public static void onPlayerWakeUp(PlayerWakeUpEvent event) {
-		// TODO: confirm wake immediately is the new updateworld()
-		// TODO: this confuses eclipse so it thinks there is a memory leak.
-        if (!event.getEntity().level().isClientSide && !event.wakeImmediately()) {
-			event.getEntity().heal((float) MyConfig.getWakeupHealingAmount());
-        }
+
+		Player e = event.getEntity();
+		
+		if (e.level().isClientSide()) 
+			return;
+		
+		if (!e.level().isDay())
+			return;
+
+		event.getEntity().heal((float) MyConfig.getWakeupHealingAmount());
+
 	}
 }
