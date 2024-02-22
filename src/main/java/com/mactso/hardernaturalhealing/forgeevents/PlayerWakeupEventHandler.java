4bfb8db09b2dca011c5438e206c1d3cd7b691fa5
Mac <Mac@Coke.lan>
2024-02-21 18:01:28 -0600

    Version 14.

--- src/main/java/com/mactso/hardernaturalhealing/forgeevents/PlayerWakeupEventHandler.java
+++ src/main/java/com/mactso/hardernaturalhealing/forgeevents/PlayerWakeupEventHandler.java
@@ -2,6 +2,7 @@ package com.mactso.hardernaturalhealing.forgeevents;
 
 import com.mactso.hardernaturalhealing.config.MyConfig;
 
+import net.minecraft.world.entity.player.Player;
 import net.minecraftforge.event.entity.player.PlayerWakeUpEvent;
 import net.minecraftforge.eventbus.api.SubscribeEvent;
 import net.minecraftforge.fml.common.Mod;
@@ -9,11 +10,19 @@ import net.minecraftforge.fml.common.Mod;
 @Mod.EventBusSubscriber()
 public class PlayerWakeupEventHandler {
 
+//	@SuppressWarnings("resource")
 	@SubscribeEvent
 	public static void onPlayerWakeUp(PlayerWakeUpEvent event) {
-		// TODO: confirm wake immediately is the new updateworld()
-        if (!event.getEntity().level.isClientSide && !event.wakeImmediately()) {
-			event.getEntity().heal((float) MyConfig.getWakeupHealingAmount());
-        }
+
+		Player e = event.getEntity();
+		
+		if (e.level.isClientSide()) 
+			return;
+		
+		if (!e.level.isDay())
+			return;
+
+		e.heal((float) MyConfig.getWakeupHealingAmount());
+
 	}
 }
