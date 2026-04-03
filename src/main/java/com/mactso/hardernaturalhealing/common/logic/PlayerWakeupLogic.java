b0aaf53e99267bc3464f8f9f62ce458cce018a97
Mac <Mac@Coke.lan>
2026-04-03 12:49:25 -0500

    v1.16 for Neoforge Minecraft 26.1

--- src/main/java/com/mactso/hardernaturalhealing/common/logic/PlayerWakeupLogic.java
+++ src/main/java/com/mactso/hardernaturalhealing/common/logic/PlayerWakeupLogic.java
@@ -1,6 +1,14 @@
 package com.mactso.hardernaturalhealing.common.logic;
 
+import net.minecraft.core.Holder.Reference;
+import net.minecraft.resources.ResourceKey;
+import net.minecraft.server.level.ServerLevel;
 import net.minecraft.world.entity.player.Player;
+import net.minecraft.world.timeline.Timeline;
+import net.minecraft.world.timeline.Timelines;
+
+import java.util.Optional;
+
 import com.mactso.hardernaturalhealing.modloader.config.MyConfig;
 
 public class PlayerWakeupLogic {
@@ -14,10 +22,32 @@ public class PlayerWakeupLogic {
     public static void handleWakeup(Player player) {
         // Skip client side
         if (player.level().isClientSide()) return;
-
+        ServerLevel serverLevel = (ServerLevel) player.level();
+        // note that this is 'kludgy' since it doesn't use the new TimeLine approach.
         // Only heal if it's daytime
-        if (player.level().getDayTime()%24000 > 20)
+        long t1 = player.level().getDefaultClockTime();  // 0 to 24000 
+        long t2 = player.level().getOverworldClockTime();  // 0 to 24000
+        long t3 = player.level().getGameTime();  // this is unadjusted ticks since minecraft started.
+
+        if (t1%24000 > 20)
         	return;
+        
+        // this is more along the proper lines of using hte Timelines code.  I'm just not going ot use
+        // it yet.
+        
+        ResourceKey<Timeline> tod = Timelines.OVERWORLD_DAY;
+        
+        Optional<Reference<Timeline>> todra = serverLevel.registryAccess().get(Timelines.OVERWORLD_DAY);
+        if (todra.isPresent()) {
+             Reference<Timeline> dayTimeline = todra.get();
+             if (dayTimeline.value() != null) {
+                 int currentDay = dayTimeline.value().getPeriodCount(serverLevel.clockManager());
+                 long currentTick = dayTimeline.value().getCurrentTicks(serverLevel.clockManager());
+                 long currentTotalTick = dayTimeline.value().getTotalTicks(serverLevel.clockManager());
+                 int debug = 5;
+                		 
+             }
+        }
 
         // Heal player using config value
         float healingAmount = (float) MyConfig.getWakeupHealingAmount();
